13960e1fee0401480b45d51896134a981af96e3a
Sébastien Huss <sebastien.huss@gmail.com>
2021-10-29 17:18:10 +0200

    Ajout de la taille normale

--- src/sub/size.js
+++ src/sub/size.js
@@ -3,22 +3,27 @@ export default function(obj, me) {
 	obj.getSize	= function() { return sz }
 	obj.defaultSize	= function() {
 		sz='';
-		if(me)  me.classed('is-small',false).classed('is-medium',false).classed('is-large',false)
+		if(me)  me.classed('is-small',false).classed('is-medium',false).classed('is-large',false).classed('is-normal',false)
 		return obj
 	}
 	obj.small	= function() {
 		sz='is-small';
-		if(me)  me.classed('is-small',true).classed('is-medium',false).classed('is-large',false)
+		if(me)  me.classed('is-small',true).classed('is-medium',false).classed('is-large',false).classed('is-normal',false)
+		return obj
+	}
+	obj.normalSize	= function() {
+		sz='is-normal';
+		if(me)  me.classed('is-small',false).classed('is-medium',false).classed('is-large',false).classed('is-normal',true)
 		return obj
 	}
 	obj.medium	= function() {
 		sz='is-medium';
-		if(me)  me.classed('is-small',false).classed('is-medium',true).classed('is-large',false)
+		if(me)  me.classed('is-small',false).classed('is-medium',true).classed('is-large',false).classed('is-normal',false)
 		return obj
 	}
 	obj.large	= function() {
 		sz='is-large';
-		if(me)  me.classed('is-small',false).classed('is-medium',false).classed('is-large',true)
+		if(me)  me.classed('is-small',false).classed('is-medium',false).classed('is-large',true).classed('is-normal',false)
 		return obj
 	}
 	return obj
